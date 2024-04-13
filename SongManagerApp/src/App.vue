f1b6977ac10e24adffbce5a1a3fb36cb39b64452
neonabuko <matheusventureli50@gmail.com>
2024-04-13 20:13:40 -0300

    chore/fix docker configuration

--- SongManagerApp/src/App.vue
+++ SongManagerApp/src/App.vue
@@ -1,51 +1,64 @@
 <template>
-  <div class="upload-form">
-    <form @submit.prevent="uploadSongAsync" method="post" enctype="multipart/form-data">
-      <input type="file" name="file" accept=".mp3" ref="fileInput">
-      <input type="submit" value="Upload">
-    </form>
-  </div>
-  <div class="song-player">
-    <audio controls>
-      <source src="" type="audio/mpeg"
-    </audio>
-  </div>
+  <main class="container">
+    <div class="app-header">
+      <h1>Song Manager App</h1>
+    </div>
+
+    <div class="upload-form">
+      <form @submit.prevent="uploadSongAsync" method="post" enctype="multipart/form-data">
+        <input type="file" name="file" accept=".mp3" ref="fileInput">
+        <input type="submit" value="Upload">
+        <h3 v-if="uploading">Uploading...</h3>
+      </form>
+    </div>
+
+    <div class="audio-grid">
+      <div class="audio-player shadow" v-for="song in songs">
+        <h5>{{ song.name }}</h5>
+        <audio controls>
+          <source :src="song.url" type="audio/mpeg">
+        </audio>
+      </div>
+    </div>
+  </main>
+
 </template>
 
 <script>
 import { API_URL } from './scripts/variables';
 import axios from 'axios'
+import Navbar from './components/Navbar.vue';
 
 export default {
   data() {
     return {
-      song: ''
+      songs: [],
+      uploading: false
     }
   },
+  components: {
+      Navbar: Navbar
+    },
   methods: {
     async uploadSongAsync() {
+      this.uploading = true
       let fileInput = this.$refs.fileInput;
       if (fileInput.files.length > 0) {
         let formData = new FormData();
         formData.append('file', fileInput.files[0]);
-
-        try {
-          await axios.post(API_URL + '/upload', formData);
-          console.log('Upload successful');
-        } catch (error) {
-          console.error('Error uploading file:', error);
-        }
-      } else {
-        console.log('No file selected.');
+        await axios.post(API_URL + '/upload', formData).then(async () => await this.fetchAllSongsAsync())
+        this.uploading = false
       }
     },
-    async getHome() {
-      await axios.get(API_URL).then((response) => {
-        console.log("Status:", response.status);
-      })
-    }
-  }, mounted() {
-    this.getHome()
+    async fetchAllSongsAsync() {
+      await axios.get(API_URL + '/songs').then(async (response) => {
+        this.songs = await response.data
+        console.log('ok');
+      } 
+      )}
+  },
+  mounted() {
+    this.fetchAllSongsAsync()
   }
 }
 </script>
