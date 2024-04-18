8ad1174f81534457782edffc7dbc4f99a495760a
neonabuko <matheusventureli50@gmail.com>
2024-04-18 06:55:00 -0300

    Put footer in Home.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <navbar></navbar>
   <router-view></router-view>
-  <div class="footer-div">
-    <footer class="app-footer">{{songs.length}} songs</footer>
-  </div>
 </template>
 
 <script>
-import { mapState } from 'vuex/dist/vuex.cjs.js';
 import Navbar from './components/Navbar.vue'
 
 export default {
@@ -15,9 +11,6 @@ export default {
     return {
       uploading: false,
     }
-  },
-  computed: {
-    ...mapState(['songs'])
   },
   components: {
     navbar: Navbar
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,22 @@
 <template>
   <main class="mt-4">
     <AudioRow></AudioRow>
+    <div class="footer-div">
+      <footer class="app-footer">{{ songs.length }} songs</footer>
+    </div>
   </main>
 </template>
 
 <script>
 import AudioRow from '../components/AudioRow.vue'
+import { mapState } from 'vuex/dist/vuex.cjs.js';
 
 export default {
-    components: {
-        AudioRow: AudioRow
-    }
+  computed: {
+    ...mapState(['songs'])
+  },
+  components: {
+    AudioRow: AudioRow
+  }
 }
 </script>
