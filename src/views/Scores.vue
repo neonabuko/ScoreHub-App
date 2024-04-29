90e6253fd0e8fd711da27169912d6c3a7038ed23
neonabuko <matheusventureli50@gmail.com>
2024-04-28 22:24:46 -0300

    Add render musicxml to svg functionality using verovio

--- src/views/Scores.vue
+++ src/views/Scores.vue
@@ -1,26 +1,94 @@
 <template>
-    <div id="notation" class="bg-white"></div>
+    <div>
+        <div class="navigation">
+            <button class="btn" @click="prevPage" :disabled="currentPage <= 1">Previous</button>
+            <span>Page {{ currentPage }} of {{ totalPages }}</span>
+            <button class="btn" @click="nextPage" :disabled="currentPage >= totalPages">Next</button>
+        </div>
+        <div id="notation" class="bg-white"></div>
+    </div>
 </template>
 
 <script>
+import createVerovioModule from 'verovio/wasm';
+import { VerovioToolkit } from 'verovio/esm';
 
 export default {
-    mounted() {
-        let tk = new verovio.toolkit();
-        fetch("https://www.verovio.org/examples/downloads/Schubert_Lindenbaum.mei")
-            .then((response) => response.text())
-            .then((meiXML) => {
-                tk.setOptions({
-                    scale: 60,
-                    landscape: true,
-                    adjustPageWidth: true
-                })
-                let svg = tk.renderData(meiXML, {});
-                document.getElementById("notation").innerHTML = svg;
-            });
-    }
-}
+    data() {
+        return {
+            verovioToolkit: null,
+            currentPage: 1,
+            totalPages: 0,
+        };
+    },
+    methods: {
+        async fetchScore() {
+            try {
+                const VerovioModule = await createVerovioModule();
+                this.verovioToolkit = new VerovioToolkit(VerovioModule);
 
+                const response = await fetch('/credo.musicxml');
+
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch MusicXML: ${response.statusText}`);
+                }
+
+                const musicXML = await response.text();
+
+                this.verovioToolkit.loadData(musicXML, { format: 'xml' });
+
+                this.totalPages = this.verovioToolkit.getPageCount();
+
+                this.renderPage(this.currentPage);
+            } catch (error) {
+                console.error('Error in fetchScore:', error);
+            }
+        },
+        renderPage(pageNumber) {
+            const svg = this.verovioToolkit.renderToSVG(pageNumber, {});
+            const notationDiv = document.getElementById('notation');
+            notationDiv.innerHTML = svg;
+        },
+        nextPage() {
+            if (this.currentPage < this.totalPages) {
+                this.currentPage++;
+                this.renderPage(this.currentPage);
+            }
+        },
+        prevPage() {
+            if (this.currentPage > 1) {
+                this.currentPage--;
+                this.renderPage(this.currentPage);
+            }
+        },
+    },
+    mounted() {
+        this.fetchScore();
+    },
+};
 </script>
 
-<style></style>
+<style>
+#notation {
+    text-align: center;
+}
+
+#notation svg {
+    width: 100%;
+    height: 100%;
+    max-width: fit-content;
+    max-height: fit-content;
+}
+
+.navigation {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: rgb(83, 34, 125);
+}
+
+button:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+}
+</style>
